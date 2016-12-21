b138a7049985e554ddc2a8a83e6c6eb80ebf5864
Hironori Oka <hironorioka28@gmail.com>
2016-12-21 23:28:54 +0900

    program

--- wp-content/themes/hirowordpress/index.php
+++ wp-content/themes/hirowordpress/index.php
@@ -5,7 +5,7 @@
 <title>hirowordpress</title>
 <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0, user-scalable=no">
 <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-<link rel="stylesheet" href="<?php bloginfo('template_url')?>/style.css">
+<link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/style.css">
 <?php wp_head(); ?>
 </head>
 <body class="page">
@@ -19,52 +19,19 @@
       <section class="update">
         <h2 class="update__title">RECENT UPDATE</h2>
         <div class="update__list">
+          <?php query_posts('posts_per_page = 5'); ?>
           <ul class="updateList">
-            <li class="updateList__item">
-              <a class="updateList__link" href="#">
-                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
-                <div class="updateList__titleWrapper">
-                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
-                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
-                </div>
-              </a>
-            </li>
-            <li class="updateList__item">
-              <a class="updateList__link" href="#">
-                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
-                <div class="updateList__titleWrapper">
-                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
-                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
-                </div>
-              </a>
-            </li>
-            <li class="updateList__item">
-              <a class="updateList__link" href="#">
-                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
-                <div class="updateList__titleWrapper">
-                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
-                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
-                </div>
-              </a>
-            </li>
-            <li class="updateList__item">
-              <a class="updateList__link" href="#">
-                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
-                <div class="updateList__titleWrapper">
-                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
-                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
-                </div>
-              </a>
-            </li>
-            <li class="updateList__item">
-              <a class="updateList__link" href="#">
-                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
-                <div class="updateList__titleWrapper">
-                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
-                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
-                </div>
-              </a>
-            </li>
+            <?php while (have_posts()) : the_post(); ?>
+              <li class="updateList__item">
+                <a class="updateList__link" href="<?php the_permalink(); ?>">
+                  <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
+                  <div class="updateList__titleWrapper">
+                    <h3 class="updateList__title"><?php the_title(); ?></h3>
+                    <p class="updateList__date"><date><?php the_time('Y/m/d H:i'); ?></date></p>
+                  </div>
+                </a>
+              </li>
+            <?php endwhile; ?>
           </ul>
         </div><!-- /.update__list -->
         <a class="update__more" href="#">MORE</a>
