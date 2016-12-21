07a965a1043c2aa6153f4217a2a01c2675a309b0
Hironori Oka <hironorioka28@gmail.com>
2016-12-21 22:24:03 +0900

    new theme

---
+++ wp-content/themes/hirowordpress/index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>hirowordpress</title>
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0, user-scalable=no">
+<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="page">
+  <section class="page__wrapper">
+    <div class="page__header">
+      <header class="header">
+        <h1 class="header__title">hirowordpress</h1>
+      </header>
+    </div>
+    <div class="page__contents">
+      <section class="update">
+        <h2 class="update__title">RECENT UPDATE</h2>
+        <div class="update__list">
+          <ul class="updateList">
+            <li class="updateList__item">
+              <a class="updateList__link" href="#">
+                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
+                <div class="updateList__titleWrapper">
+                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
+                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
+                </div>
+              </a>
+            </li>
+            <li class="updateList__item">
+              <a class="updateList__link" href="#">
+                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
+                <div class="updateList__titleWrapper">
+                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
+                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
+                </div>
+              </a>
+            </li>
+            <li class="updateList__item">
+              <a class="updateList__link" href="#">
+                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
+                <div class="updateList__titleWrapper">
+                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
+                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
+                </div>
+              </a>
+            </li>
+            <li class="updateList__item">
+              <a class="updateList__link" href="#">
+                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
+                <div class="updateList__titleWrapper">
+                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
+                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
+                </div>
+              </a>
+            </li>
+            <li class="updateList__item">
+              <a class="updateList__link" href="#">
+                <div class="updateList__imgWrapper" style="background-image:url(http://res.cloudinary.com/hiro28/image/upload/v1482243634/IMG_2089_pl0myg.jpg)"></div>
+                <div class="updateList__titleWrapper">
+                  <h3 class="updateList__title">＜世界キャラさみっと＞キャラ４００体集合で開幕　ふなっしーも共演</h3>
+                  <p class="updateList__date"><date>2016/12/18 12:00</date></p>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div><!-- /.update__list -->
+        <a class="update__more" href="#">MORE</a>
+      </section>
+    </div><!-- /.page__contents -->
+    <div class="page__footer">
+      <footer class="footer">
+        <p class="footer__text">&copy; hirowordpress</p>
+      </footer>
+    </div>
+  </section><!-- /.page__wrapper -->
+</body>
+</html>
